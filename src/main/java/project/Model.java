6db1f7d2f7fd27e6afb59bc102d98d620a64c4f2
Zaylet Feliciano <zayletfm@gmail.com>
2022-09-04 22:25:59 -0400

    built getters

--- src/main/java/project/Model.java
+++ src/main/java/project/Model.java
@@ -2,7 +2,17 @@ package project;
 
 public class Model {
 
-    private String num;
+    private String age;
     private String name;
     private String lastName;
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
 }
